f8866185354fba610d9d86cc59aa6ea0478cb57c
frank <team@frank.com>
2023-09-19 08:32:39 -0600

    modificados:     resources/views/livewire/book/books.blade.php 	modificados:     resources/views/livewire/book/partials/create.blade.php

--- resources/views/livewire/book/books.blade.php
+++ resources/views/livewire/book/books.blade.php
@@ -1,10 +1,61 @@
-   
-        <div class="content">
-                
-                    @include('livewire.book.partials.create') <!-- Vista parcial de creación de libro -->
-                
-                    @include('livewire.book.partials.edit') <!-- Vista parcial de edición de libro -->
-               
-                    @include('livewire.book.partials.delete') <!-- Vista parcial de eliminación de libro -->
-                
+<div id="books">
+    <h1>Libros</h1>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Código de barras</th>
+                        <th>Título</th>
+                        <th>Autor</th>
+                        <th>Edición</th>
+                        <th>Área</th>
+                        <th>Casa editorial</th>
+                        <th>Cantidad</th>
+                        <th>Origen</th>
+                        <th>Foto</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($books as $book)
+                    <tr>
+                        <td>{{ $book->barcode }}</td>
+                        <td>{{ $book->title }}</td>
+                        <td>{{ $book->author }}</td>
+                        <td>{{ $book->edition }}</td>
+                        <td>{{ $book->area }}</td>
+                        <td>{{ $book->publishing_house }}</td>
+                        <td>{{ $book->quantity }}</td>
+                        <td>{{ $book->origin }}</td>
+                        <td>
+                            <img src="{{ $book->photo }}" alt="{{ $book->title }}" width="100" />
+                        </td>
+                        <td>
+                            <button wire:click="edit({{ $book->id }})" class="btn btn-primary">Editar</button>
+                            <button wire:click="delete({{ $book->id }})" class="btn btn-danger">Eliminar</button>
+                        </td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="10">No hay libros</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="float-right">
+                <button wire:click="create()">Crear libro</button>
+            </div>
+        </div>
+    </div>
+    
+    
+    @include('livewire.book.partials.create')
+    @include('livewire.book.partials.edit')
+</div>
--- resources/views/livewire/book/partials/create.blade.php
+++ resources/views/livewire/book/partials/create.blade.php
@@ -1,70 +1,46 @@
 <div>
     <h2>Crear Libro</h2>
-    <div class="container">
-        <table class="table table-bordered">
-            <tbody>
-                <tr>
-                    <th>Código de Barras:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="barcode">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Título:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="title">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Autor:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="author">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Edición:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="edition">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Área:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="area">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Casa Editorial:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="publishing_house">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Comentario:</th>
-                    <td>
-                        <textarea class="form-control" wire:model="comment"></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <th>Cantidad:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="quantity">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Origen:</th>
-                    <td>
-                        <input type="text" class="form-control" wire:model="origin">
-                    </td>
-                </tr>
-                <tr>
-                    <th>Foto:</th>
-                    <td>
-                        <input type="file" class="form-control-file" wire:model="photo">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <button type="submit" class="btn btn-primary">Guardar</button>
+    <form wire:submit.prevent="create">
+        <div class="form-group">
+            <label for="barcode">Código de Barras:</label>
+            <input type="text" class="form-control" id="barcode" wire:model="barcode">
+        </div>
+        <div class="form-group">
+            <label for="title">Título:</label>
+            <input type="text" class="form-control" id="title" wire:model="title">
+        </div>
+        <div class="form-group">
+            <label for="author">Autor:</label>
+            <input type="text" class="form-control" id="author" wire:model="author">
+        </div>
+        <div class="form-group">
+            <label for="edition">Edición:</label>
+            <input type="text" class="form-control" id="edition" wire:model="edition">
+        </div>
+        <div class="form-group">
+            <label for="area">Área:</label>
+            <input type="text" class="form-control" id="area" wire:model="area">
+        </div>
+        <div class="form-group">
+            <label for="publishing_house">Casa Editorial:</label>
+            <input type="text" class="form-control" id="publishing_house" wire:model="publishing_house">
+        </div>
+        <div class="form-group">
+            <label for="comment">Comentario:</label>
+            <input type="text" class="form-control" id="comment" wire:model="comment">
+        </div>
+        <div class="form-group">
+            <label for="quantity">Cantidad:</label>
+            <input type="number" class="form-control" id="quantity" wire:model="quantity">
+        </div>
+        <div class="form-group">
+            <label for="origin">Origen:</label>
+            <input type="text" class="form-control" id="origin" wire:model="origin">
+        </div>
+        <div class="form-group">
+            <label for="photo">Foto:</label>
+            <input type="file" class="form-control" id="photo" wire:model="photo">
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+</form>
 </div>
