f9423d17b60321c28750a625482353bfdecd8ff9
frank <team@frank.com>
2023-09-19 06:23:02 -0600

    modificados:     resources/views/livewire/book/books.blade.php

--- resources/views/livewire/book/books.blade.php
+++ resources/views/livewire/book/books.blade.php
@@ -3,12 +3,12 @@
         <h1>Libros</h1>
         <!-- Contenido principal de la página -->
         <div class="content">
-                @if($view === 'create')
+                
                     @include('livewire.book.partials.create') <!-- Vista parcial de creación de libro -->
-                @elseif($view === 'edit')
+                
                     @include('livewire.book.partials.edit') <!-- Vista parcial de edición de libro -->
-                @elseif($view === 'delete')
+               
                     @include('livewire.book.partials.delete') <!-- Vista parcial de eliminación de libro -->
-                @endif
+                
             </div>
         </div>
