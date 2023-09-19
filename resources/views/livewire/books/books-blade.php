5e7680ee2947befc6400275c8aaad5dfa9c6029a
frank <team@frank.com>
2023-09-19 03:44:50 -0600

    modificados:     resources/views/livewire/books/books-blade.php

--- resources/views/livewire/books/books-blade.php
+++ resources/views/livewire/books/books-blade.php
@@ -1,24 +1,14 @@
 <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                {{-- Contenido principal de la página de libros --}}
-                <h1>Lista de Libros</h1>
-                {{-- Aquí puedes mostrar la lista de libros o cualquier otro contenido principal --}}
-            </div>
-            <div class="col-md-4">
-                {{-- Columna lateral para acciones de libros --}}
-                @if($currentAction == 'create')
+    <div class="row justify-content-center">
+               
                     {{-- Mostrar la vista parcial para crear un libro --}}
                     @include('livewire.books.partials.create')
-                @elseif($currentAction == 'edit')
+               
                     {{-- Mostrar la vista parcial para editar un libro --}}
                     @include('livewire.books.partials.edit')
-                @elseif($currentAction == 'delete')
+               
                     {{-- Mostrar la vista parcial para eliminar un libro --}}
                     @include('livewire.books.partials.delete')
-                @else
-                    {{-- Por defecto, no se muestra ninguna vista parcial --}}
-                @endif
-            </div>
-        </div>
+              
     </div>
+</div>
