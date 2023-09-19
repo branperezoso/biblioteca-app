b6473b538b990c055a4dbae6ad07a6935d3b6196
frank <team@frank.com>
2023-09-19 02:45:25 -0600

    modificados:     resources/views/livewire/book/books.blade.php

--- resources/views/livewire/book/books.blade.php
+++ resources/views/livewire/book/books.blade.php
@@ -1,3 +1,19 @@
-<div>
-    {{-- The whole world belongs to you. --}}
-</div>
+<div id="app">
+        <div class="container">
+            <!-- Barra de navegación u otros elementos comunes aquí -->
+
+            <!-- Contenido principal -->
+            <main class="py-4">
+                @if($currentAction == 'list')
+                    <!-- Mostrar la lista de libros -->
+                    @include('livewire.books.list')
+                @elseif($currentAction == 'create')
+                    <!-- Mostrar el formulario de creación de libro -->
+                    @include('livewire.books.create')
+                @elseif($currentAction == 'edit')
+                    <!-- Mostrar el formulario de edición de libro -->
+                    @include('livewire.books.edit')
+                @endif
+            </main>
+        </div>
+    </div>
