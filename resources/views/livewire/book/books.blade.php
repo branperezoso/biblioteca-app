2a8805c8c08169af2815eb7545b1c00f0020bce7
frank <team@frank.com>
2023-09-19 05:59:29 -0600

    modificados:     resources/views/livewire/book/books.blade.php

--- resources/views/livewire/book/books.blade.php
+++ resources/views/livewire/book/books.blade.php
@@ -1,22 +1,14 @@
-{{-- resources/views/livewire/book/books.blade.php --}}
-
-<div>
-    <h1>Libros</h1>
-
-    <!-- Botones para cambiar la vista principal -->
-    <div class="mb-4">
-        <button wire:click="showList">Lista de Libros</button>
-        <button wire:click="showCreate">Crear Libro</button>
-    </div>
-
-    <!-- Contenido principal de la página -->
-    <div class="content">
-        @if($view === 'create')
-            @include('livewire.book.partials.create') <!-- Vista parcial de creación de libro -->
-        @elseif($view === 'edit')
-            @include('livewire.book.partials.edit') <!-- Vista parcial de edición de libro -->
-        @elseif($view === 'delete')
-            @include('livewire.book.partials.delete') <!-- Vista parcial de eliminación de libro -->
-        @endif
-    </div>
-</div>
+            {{-- resources/views/livewire/book/books.blade.php --}}
+    <div>
+        <h1>Libros</h1>
+        <!-- Contenido principal de la página -->
+        <div class="content">
+                @if($view === 'create')
+                    @include('livewire.book.partials.create') <!-- Vista parcial de creación de libro -->
+                @elseif($view === 'edit')
+                    @include('livewire.book.partials.edit') <!-- Vista parcial de edición de libro -->
+                @elseif($view === 'delete')
+                    @include('livewire.book.partials.delete') <!-- Vista parcial de eliminación de libro -->
+                @endif
+            </div>
+        </div>
