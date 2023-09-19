2f5c67ce0a02e59a03ca50cac00532319923ded0
frank <team@frank.com>
2023-09-19 03:04:04 -0600

    modificados:     resources/views/livewire/books/partials/create-blade.php 	modificados:     resources/views/livewire/books/partials/edit.blade.php 	borrados:        resources/views/livewire/books/partials/list.blade.php 	nuevos archivos: resources/views/livewire/books/partials/update.blade.php

--- resources/views/livewire/books/partials/create-blade.php
+++ resources/views/livewire/books/partials/create-blade.php
@@ -1,8 +1,46 @@
 <div>
     <h2>Crear Libro</h2>
     <form wire:submit.prevent="create">
-        <!-- Campos del formulario para crear un libro -->
-        <!-- Asegúrate de tener los campos necesarios aquí -->
+        <div class="form-group">
+            <label for="barcode">Código de Barras:</label>
+            <input type="text" class="form-control" id="barcode" wire:model="barcode">
+        </div>
+        <div class="form-group">
+            <label for="title">Título:</label>
+            <input type="text" class="form-control" id="title" wire:model="title">
+        </div>
+        <div class="form-group">
+            <label for="author">Autor:</label>
+            <input type="text" class="form-control" id="author" wire:model="author">
+        </div>
+        <div class="form-group">
+            <label for="edition">Edición:</label>
+            <input type="text" class="form-control" id="edition" wire:model="edition">
+        </div>
+        <div class="form-group">
+            <label for="area">Área:</label>
+            <input type="text" class="form-control" id="area" wire:model="area">
+        </div>
+        <div class="form-group">
+            <label for="publishing_house">Casa Editorial:</label>
+            <input type="text" class="form-control" id="publishing_house" wire:model="publishing_house">
+        </div>
+        <div class="form-group">
+            <label for="comment">Comentario:</label>
+            <input type="text" class="form-control" id="comment" wire:model="comment">
+        </div>
+        <div class="form-group">
+            <label for="quantity">Cantidad:</label>
+            <input type="number" class="form-control" id="quantity" wire:model="quantity">
+        </div>
+        <div class="form-group">
+            <label for="origin">Origen:</label>
+            <input type="text" class="form-control" id="origin" wire:model="origin">
+        </div>
+        <div class="form-group">
+            <label for="photo">Foto:</label>
+            <input type="file" class="form-control" id="photo" wire:model="photo">
+        </div>
         <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
 </div>
