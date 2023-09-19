0637f92024b5290e008c6f0d5e84cbf0fc7279e2
frank <team@frank.com>
2023-09-19 07:57:16 -0600

    modificados:     composer.lock 	modificados:     resources/views/livewire/book/books.blade.php 	modificados:     resources/views/livewire/book/partials/create.blade.php

--- resources/views/livewire/book/books.blade.php
+++ resources/views/livewire/book/books.blade.php
@@ -1,9 +1,4 @@
-            {{-- resources/views/livewire/book/books.blade.php --}}
-    <div>
-        <h1>Libros</h1>
-        <button wire:click="create">Crear Libro</button>
-
-        <!-- Contenido principal de la página -->
+   
         <div class="content">
                 
                     @include('livewire.book.partials.create') <!-- Vista parcial de creación de libro -->
@@ -12,5 +7,4 @@
                
                     @include('livewire.book.partials.delete') <!-- Vista parcial de eliminación de libro -->
                 
-            </div>
         </div>
