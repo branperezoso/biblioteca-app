bfe123602fdcd5a1b81f1108b65250f966ef69ab
frank <team@frank.com>
2023-09-19 03:08:59 -0600

    modificados:     resources/views/livewire/books/books.blade.php

--- resources/views/livewire/books/books.blade.php
+++ resources/views/livewire/books/books.blade.php
@@ -1,19 +1,24 @@
-<div id="app">
-        <div class="container">
-            <!-- Barra de navegación u otros elementos comunes aquí -->
-
-            <!-- Contenido principal -->
-            <main class="py-4">
-                @if($currentAction == 'list')
-                    <!-- Mostrar la lista de libros -->
-                    @include('livewire.books.list')
-                @elseif($currentAction == 'create')
-                    <!-- Mostrar el formulario de creación de libro -->
+<div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                {{-- Contenido principal de la página de libros --}}
+                <h1>Lista de Libros</h1>
+                {{-- Aquí puedes mostrar la lista de libros o cualquier otro contenido principal --}}
+            </div>
+            <div class="col-md-4">
+                {{-- Columna lateral para acciones de libros --}}
+                @if($currentAction == 'create')
+                    {{-- Mostrar la vista parcial para crear un libro --}}
                     @include('livewire.books.create')
                 @elseif($currentAction == 'edit')
-                    <!-- Mostrar el formulario de edición de libro -->
+                    {{-- Mostrar la vista parcial para editar un libro --}}
                     @include('livewire.books.edit')
+                @elseif($currentAction == 'delete')
+                    {{-- Mostrar la vista parcial para eliminar un libro --}}
+                    @include('livewire.books.delete')
+                @else
+                    {{-- Por defecto, no se muestra ninguna vista parcial --}}
                 @endif
-            </main>
+            </div>
         </div>
     </div>
