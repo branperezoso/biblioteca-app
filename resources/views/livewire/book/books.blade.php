b886f868e43e92184a81d05d9c4ef8bbdb1b0938
frank <team@frank.com>
2023-09-19 07:03:41 -0600

    modificados:     resources/views/livewire/book/books.blade.php 	modificados:     routes/web.php

--- resources/views/livewire/book/books.blade.php
+++ resources/views/livewire/book/books.blade.php
@@ -1,5 +1,11 @@
-<div class="container">
-    <div class="row justify-content-center">
+            {{-- resources/views/livewire/book/books.blade.php --}}
+    <div>
+        <h1>Libros</h1>
+        <button wire:click="create">Crear Libro</button>
+
+        <!-- Contenido principal de la página -->
+        <div class="content">
+                
                     @include('livewire.book.partials.create') <!-- Vista parcial de creación de libro -->
                 
                     @include('livewire.book.partials.edit') <!-- Vista parcial de edición de libro -->
